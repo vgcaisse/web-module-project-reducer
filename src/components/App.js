b07ca7d592d5c4d5b8d5c316f774fbfc115dbea6
Visnu <vgcaisse@gmail.com>
2022-03-21 22:35:26 -0400

    got my add one to work

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,11 @@ import { addOne } from '../actions';
 
 function App() {
   const [ state, dispatch ] = useReducer(reducer, initialState);
+
+  const handleAddOne = () => {
+    // console.log('yo')
+    dispatch(addOne())
+  }
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -33,7 +38,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1} onClick={addOne}/>
+              <CalcButton value={1} onClick={handleAddOne}/>
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
